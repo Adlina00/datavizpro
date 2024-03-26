67eb69cef355b76b5958d55f30daef59f5c40c75
Adlina Amran <adlina.amran1902@gmail.com>
2024-03-26 15:12:03 +0800

    Update datavpai.py

--- datavpai.py
+++ datavpai.py
@@ -186,28 +186,13 @@ st.set_option("deprecation.showPyplotGlobalUse", False)
 @st.cache_resource
 def display_app_header():
     """Display the header of the Streamlit app."""
-    st.title("1️⃣ One-Prompt Charts 📊 ")
-    st.markdown("***AI-Powered Data Visualization Tool** ✨")
+    st.title("DataVizPro 📊 ")
+    st.markdown("**AI-Powered Data Visualization Tool** ✨")
 
 
 # Display the header of the app
 display_app_header()
 
-with st.expander("App Overview", expanded=False):
-    st.markdown(
-        """
-
-    You will find each functions either in the library or in the main script. Feel free to modu
-    - **App Header:** The function `display_app_header` defines the title and a brief description of the app, setting the context for the user. This function is displayed at the top of the app when called on line 24.
-       
-    - **API Key Check:** The `key_check` function is invoked to ensure the necessary API keys are present before proceeding. This might be for authentication or to access certain services.
-    
-    - **Data Upload and Display:** The app provides the user with an option to upload data using the `get_data` function (line 47). Once the data is uploaded, it's optionally displayed in an expandable section for the user to review.
-    
-    - **OpenAI Query Handling:** If the uploaded data is not empty, the `handle_openai_query` function is called (line 64) to process the user's prompt regarding the data and visualize it accordingly. If the uploaded data is empty, a warning is displayed to the user.  
-    
-    """
-    )
 
 # Check for the necessary API keys
 # key_check() # A function to deal with KEY availability
@@ -215,7 +200,6 @@ with st.expander("App Overview", expanded=False):
 
 if load_dotenv():
     openai_api_key = os.getenv("OPENAI_API_KEY")
-
   
     options = st.radio(
         "Data Usage", options=["Upload file", "Use Data in Storage"], horizontal=True
