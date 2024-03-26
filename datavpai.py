0b9bf5b06932d3f5e17a6aedb39ba027f9c6152a
Adlina Amran <adlina.amran1902@gmail.com>
2024-03-26 14:46:53 +0800

    Update datavpai.py

--- datavpai.py
+++ datavpai.py
@@ -212,24 +212,25 @@ with st.expander("App Overview", expanded=False):
 # key_check() # A function to deal with KEY availability
 # Alternatively, just using a text-input box
 
-API = openai.
-
-if API:
-  os.environ["OPENAI_API_KEY"] = API
+load_dotenv()
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+  
   
 
-  options = st.radio(
+options = st.radio(
       "Data Usage", options=["Upload file", "Use Data in Storage"], horizontal=True
   )
-  if options == "Upload file":
+if options == "Upload file":
       # Get data uploaded by the user
       df = get_data()
-  else:
+else:
       df = db.storage.dataframes.get(key="spectra-csv")
   
   
   # If data is uploaded successfully
-  if df is not None:
+if df is not None:
       # Create an expander to optionally display the uploaded data
       with st.expander("Show data"):
           st.write(df)
